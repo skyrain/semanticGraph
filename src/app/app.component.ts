9e8edef53b3c87000bbd33d78528571aac3ad918
root <root@DESKTOP-CMK29AD.localdomain>
2018-07-24 11:04:26 -0700

    client basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from '../../node_modules/rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Semantic Graph Explorer';
+  searchText = '';
+  searchFinished = false;
+  url = 'http://localhost:4200';
+  result = {};
+
+  constructor(private http: HttpClient) {}
+
+  onSearchClicked() {
+    console.log("function:onSearchClicked")
+    this.search(this.searchText).subscribe(
+      results => {
+        this.result = results;
+        console.log("Result: " + results);
+        this.searchFinished = true;
+      });
+  }
+
+  search(text:string): Observable<Object> {
+    console.log("Searching " + this.searchText + "...");
+    return this.http
+      .get<Object>(this.url);
+  }
 }
